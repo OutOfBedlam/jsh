42dbdcd6661b8a5ffca16ae6a84d151f26820cf2
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2025-12-18 14:51:36 +0900

    darwin ioctl

--- engine/engine_unix.go
+++ engine/engine_unix.go
@@ -97,7 +97,7 @@ func isatty(fd int) bool {
 	_, _, err := syscall.Syscall6(
 		syscall.SYS_IOCTL,
 		uintptr(fd),
-		syscall.TCGETS,
+		ioctlReadTermios,
 		uintptr(unsafe.Pointer(&termios)),
 		0, 0, 0)
 	return err == 0
